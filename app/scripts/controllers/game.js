34ef7bdd2fba2231509a523c839ce04e162631c2
Jordan <jordan.laforest@gmail.com>
2014-08-13 21:35:10 -0400

    Added basic front end player list functionality

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -11,9 +11,24 @@ var app = angular.module('drawgameApp');
 
 app.controller('GameCtrl', function ($scope) {
   $scope.messages = [];
+  $scope.players = [];
+  $scope.drawingPlayer = 0;
+
   $scope.addMessage = function(username, message){
     $scope.messages.push({name: username, text: message});
   };
+  $scope.addPlayer = function(id, username, score){
+		$scope.players.push({id: id, username: username, score: score});
+  };
+  $scope.isDrawing = function(playerId){
+    return $scope.drawingPlayer === playerId;
+  };
+
+  //Init
+	$scope.addPlayer(0, "Bob", 4);
+	$scope.addPlayer(1, "Patrick", 0);
+	$scope.addPlayer(2, "Sandy", 7);
+
 });
 
 app.controller('PlayerListCtrl', function($scope){
@@ -21,7 +36,6 @@ app.controller('PlayerListCtrl', function($scope){
 });
 
 app.controller('CanvasCtrl', function($scope){
-
 });
 
 app.controller('ChatCtrl', function($scope){
