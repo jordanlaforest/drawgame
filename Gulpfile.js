70f527642998f8ac018bb2fb1b76f09338735eb4
Nicholas Dujay <nickdujay@gmail.com>
2014-09-06 13:53:17 -0400

    Added some tasks to Gulpfile, started a tiny bit on production tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,7 +11,8 @@ var gulp = require('gulp'),
 var mainBowerFiles = require('main-bower-files'),
   express = require('express'),
   tinylr = require('tiny-lr'),
-  del = require('del');
+  del = require('del'),
+  karma = require('karma').server;
 
 /**
  * Local variables
@@ -19,6 +20,7 @@ var mainBowerFiles = require('main-bower-files'),
 var paths = {
   scripts: ['app/scripts/{,*/}*.js'],
   styles: ['app/styles/*.scss'],
+  views: ['app/views/*.html'],
   server: 'server/server.js',
 
   prodDir: 'dist/',
@@ -28,11 +30,22 @@ var PORT = 9000,
   LR_PORT = 32756;
 var lr;
 
+/**
+ * The clean task
+ */
 gulp.task('clean', function(done) {
   del.sync([paths.devDir, paths.prodDir]);
   done();
 });
 
+/**
+ * Development server tasks
+ *
+ * livereload:    starts the livereload server
+ * watch:         watches files and restarts the server
+ *                also processes index.html when it changes
+ * serve:         runs a local static file server, so we can test on a server
+ */
 gulp.task('livereload', function() {
   lr = tinylr();
   //default port
@@ -41,10 +54,14 @@ gulp.task('livereload', function() {
   });
 });
 
-//COPY PASTED FROM SOMEWHERE hehe
+// COPY PASTED FROM SOMEWHERE hehe
 // Notifies livereload of changes detected
 // by `gulp.watch()`
 function notifyLivereload(event) {
+
+  if(event === null) {
+    return;
+  }
 
   // `gulp.watch()` events provide an absolute path
   // so we need to make it relative to the server root
@@ -56,23 +73,45 @@ function notifyLivereload(event) {
     }
   });
 
+  //pretty print a message for the file changing
   g.util.log(g.util.colors.magenta(fileName), 'changed');
 }
 
-gulp.task('watch', ['livereload'], function() {
-  gulp.watch([paths.scripts, paths.styles, paths.devDir + '*.*'],
+/**
+ * Watches files and reloads the webpage
+ * requires the livereload server be started first
+ */
+gulp.task('watch', ['livereload'], function(done) {
+  gulp.watch([paths.scripts, paths.views, paths.devDir + '*.*'],
     notifyLivereload);
 
-  gulp.watch('app/index.html', ['index', notifyLivereload]);
+  gulp.watch(paths.styles, function(file) {
+    gulp.start('sass')
+      .on('task_end', function() {
+        notifyLivereload(file);
+      });
+  });
+
+  //this will update devDir index.html, and the previous trigger will occur
+  gulp.watch('app/index.html', function(file) {
+    gulp.start('index')
+      .on('task_end', function() {
+        notifyLivereload(file);
+      });
+  });
+
+  //ensure we can run watch synchronously
+  done();
 });
 
-//run index and sass first
-gulp.task('serve', ['watch', 'clean', 'index'], function() {
+// Runs a local static file server
+gulp.task('serve', ['clean', 'watch', 'index'], function() {
   //the order of serving matters, index.html will be found in paths.devDir
-  //first before searching in the app folder
+  //first before searching in the app folder, index.html in the app folder
+  //doesn't work on its own
   express()
 
-  //injects the livereload script into your stuff
+  //injects the livereload script into your pages
   .use(require('connect-livereload')({
     port: LR_PORT
   }))
@@ -83,13 +122,14 @@ gulp.task('serve', ['watch', 'clean', 'index'], function() {
   //serve the root directory
   .use(express.static('.'))
 
-  //serve the app directory (for views)
+  //serve the app directory (for view manipulation)
   .use(express.static('app/'))
 
   .listen(PORT, function() {
-    //pretty print a message as if it were from gulp (with a time prefix)
-    g.util.log('Listening on port', g.util.colors.cyan(PORT))
+    // pretty print a message as if it were from gulp (with a time prefix)
+    g.util.log('Listening on port', g.util.colors.cyan(PORT));
   });
+
   //TODO live reload with the API server.
   /*
   g.nodemon({
@@ -127,6 +167,38 @@ gulp.task('index', ['sass'], function() {
     .pipe(gulp.dest(paths.devDir));
 });
 
+gulp.task('index:dist', ['sass:dist'], function() {
+  return gulp.src('app/index.html')
+    .pipe(g.inject(
+      gulp.src(mainBowerFiles(), {
+        read: false
+      }), {
+        starttag: '<!-- inject:bower:{{ext}} -->',
+        addRootSlash: false //remove the slash in front of /bower_components
+      }
+    )) //use bower components locally
+    //replace with as many cdn links as possible
+    .pipe(g.googleCdn(require('./bower.json')))
+    .pipe(g.googleCdn(require('./bower.json'), {
+      cdn: require('cdnjs-cdn-data')
+    }))
+    .pipe(g.inject(
+      gulp.src(
+        paths.scripts.concat(
+          [paths.prodDir + '*.css']
+        ), {
+          read: false
+        }
+      ), {
+        starttag: '<!-- inject:app:{{ext}} -->',
+        addRootSlash: false,
+        ignorePath: [paths.prodDir]
+      }
+    )) //app
+    .pipe(gulp.dest(paths.prodDir));
+});
+
+
 //the return means we can use sass synchronously
 //we need to clean before running sass and index for example
 //instead of sometimes after sometimes before
@@ -136,20 +208,49 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(paths.devDir));
 });
 
+//gulp-sass is different from gulp-ruby-sass
+//gulp sass is faster, but isn't official
 gulp.task('sass:dist', function() {
   return gulp.src(paths.styles)
     .pipe(g.sass())
-    .pipe(gulp.dest(paths.prodDir));
+    .pipe(gulp.dest(paths.prodDir + 'css/'));
 });
 
-gulp.task('test', function() {
-  //TODO
+// inject files into the karma file
+gulp.task('karmaconf', function() {
+  return gulp.src('test/karma.conf.js')
+    .pipe(g.inject(
+      gulp.src(mainBowerFiles(), {
+        read: false
+      }), {
+        starttag: '// inject:bower:{{ext}}',
+        endtag: '// endinject',
+        addRootSlash: false,
+        transform: function(filepath) {
+          return '\'' + filepath + '\',';
+        }
+      }
+    ))
+    .pipe(gulp.dest(paths.devDir));
 });
 
-gulp.task('concat', function() {
+gulp.task('karma', ['karmaconf'], function() {
+  var conf = require('./' + paths.devDir + 'karma.conf.js');
+  karma.start(conf);
+});
+
+gulp.task('test', ['karma']);
+
+// sourcemaps let us view the original source in production
+// so we can debug problems
+gulp.task('scripts:dist', function() {
   return gulp.src(paths.scripts)
-    .pipe(g.concat('main.js'))
-    .pipe(gulp.dest(paths.dest));
+    .pipe(g.sourcemaps.init())
+    .pipe(g.ngAnnotate()) //annotate functions with ['$scope', ..] so uglify doesn't break everything
+    .pipe(g.concat('app-built.js')) //merge files into one file with the name 'app-built.js'
+    .pipe(g.uglify()) //obfuscate code to smaller variable names to save space
+    .pipe(g.sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.prodDir + 'js/'));
 });
 
 gulp.task('jshint', function() {
