33b00094a18cbbde7906d860d55fa6d3aa398a3d
Nicholas Dujay <nickdujay@gmail.com>
2014-09-20 19:13:46 -0400

    I am using 'concat' down below for scripts, so I decided it's easier to just revert scripts back to an array

--- Gulpfile.js
+++ Gulpfile.js
@@ -17,7 +17,7 @@ var mainBowerFiles = require('main-bower-files'),
  * Local variables
  */
 var paths = {
-  scripts: 'app/scripts/{,*/}*.js',
+  scripts: ['app/scripts/{,*/}*.js'],
   styles: 'app/styles/*.scss',
   views: 'app/views/*.html',
   server: 'server/server.js',
