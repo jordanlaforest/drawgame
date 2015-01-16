b56ec742b01c4738b55c35f7ee19e280c513d7ad
Nicholas Dujay <nickdujay@gmail.com>
2015-01-16 14:03:43 -0500

    copied client folder, started rewriting in ES6

---
+++ refactor/client/scripts/app.js
@@ -0,0 +1,38 @@
+import './angular-socket';
+
+/**
+ * @ngdoc overview
+ * @name drawgameApp
+ * @description
+ * # drawgameApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('drawgameApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'socket.io'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/game/:gameId', {
+        templateUrl: 'views/game.html',
+        controller: 'GameCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+
+import './controllers/game';
+import './controllers/main';
+import './controllers/player';
