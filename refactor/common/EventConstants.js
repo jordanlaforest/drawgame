c1fe160dc47f3930fda450f35ce23a42857ef71d
Nicholas Dujay <nickdujay@gmail.com>
2015-01-16 14:05:51 -0500

    added a constants file

---
+++ refactor/common/EventConstants.js
@@ -0,0 +1,9 @@
+export const SERVER_MESSAGE_EVENT  = 'message:server';
+export const INIT_EVENT            = 'init';
+export const NAME_CHANGE_EVENT     = 'name';
+export const CHAT_EVENT            = 'chat';
+export const CREATE_UID_EVENT      = 'uid';
+
+export const DRAW_START_EVENT      = 'draw:start';
+export const DRAW_MOVE_EVENT       = 'draw:move';
+export const DRAW_END_EVENT        = 'draw:end';
