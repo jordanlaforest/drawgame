2374b6c204db090a6cf6d352675e3f35eff107c1
Jordan Laforest <jordan.laforest@gmail.com>
2015-02-08 17:10:34 -0500

    Moved some html to the App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,8 +5,18 @@ let { RouteHandler } = Router;
 var App = React.createClass({
   render() {
     return (
-      <div>
-        <RouteHandler/>
+      <div className="container-fluid">
+        <header className="header">
+          <h3 className="text-muted">Draw Game</h3>
+        </header>
+
+        <div>
+          <RouteHandler/>
+        </div>
+
+        <footer className="footer">
+          <p>Created by Jordan Laforest and Nicholas Dujay</p>
+        </footer>
       </div>
     );
   }
