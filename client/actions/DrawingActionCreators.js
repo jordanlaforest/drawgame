f5534ee976e5c5a2c592b452465e78ac437cdb45
Nicholas Dujay <nickdujay@gmail.com>
2015-02-10 22:24:59 -0500

    Got the rest of the renaming path events to pen events. It makes much more semantic sense this way.

--- client/actions/DrawingActionCreators.js
+++ client/actions/DrawingActionCreators.js
@@ -2,9 +2,9 @@ import Marty from 'marty';
 import DrawingConstants from '../constants/DrawingConstants';
 
 var DrawingActionCreators = Marty.createActionCreators({
-  startPath: DrawingConstants.SEND_PEN_DOWN(),
-  endPath: DrawingConstants.SEND_PEN_UP(),
-  movePath: DrawingConstants.SEND_PEN_MOVE(),
+  sendPenDown: DrawingConstants.SEND_PEN_DOWN(),
+  sendPenUp: DrawingConstants.SEND_PEN_UP(),
+  sendPenMove: DrawingConstants.SEND_PEN_MOVE(),
 });
 
 export default DrawingActionCreators;
