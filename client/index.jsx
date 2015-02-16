919c3265d7cec1253ee9bd43d934316d3e6d3b65
Nicholas Dujay <nickdujay@gmail.com>
2015-02-16 01:28:05 -0500

    first move to flummox MainApp really should be empty here, but git doesn't let you stage some of the lines

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
 window.React = React;
 
-import Router from './router.jsx';
+import { Routes } from './router.jsx';
+import Router from 'react-router';
+
+import MainApp from './MainApp';
+import { Flux } from 'flummox';
+
+// hack since proto's are broken right now in babel...
+let flux = new MainApp();
+flux.__proto__ = Flux.prototype;
+
+Router.run(Routes, (Handler) => {
+  React.withContext(
+    { flux },
+    () => React.render(<Handler />, document.getElementById('react-view'))
+  );
+});
