dbb59f47d0d2014dd8729ddad63ca245cbab6aee
Jordan Laforest <jordan.laforest@gmail.com>
2021-05-03 21:11:33 -0400

    Added the UI for some drawing controls

--- client/components/DrawingControls.jsx
+++ client/components/DrawingControls.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Panel from 'react-bootstrap/lib/Panel';
+import { HexColorPicker } from 'react-colorful';
 
-class DrawingControls extends React.Component {
-  render() {
-    return (
-      <Panel>
-        <Panel.Heading>Drawing Controls</Panel.Heading>
-        <Panel.Body><h2>Drawing Controls</h2></Panel.Body>
-      </Panel>
-    );
-  }
-}
+import Icon from './Icon.jsx';
+
+import './styles/drawingControls.css';
+
+const DrawingControls = () => {
+  const [color, setColor] = useState('#000000');
+  const [size, setSize] = useState(12);
+  const iconStyle = {fontSize: size + 'px'};
+  console.log('Rendered DrawingControls with size: ' + iconStyle);
+  return (
+    <Panel className="drawingControls">
+      <Panel.Heading>Drawing Controls</Panel.Heading>
+      <Panel.Body>
+        <HexColorPicker color={color} onChange={setColor} />
+        <span className="brushSizeContainer">
+          <input type="range" min={1} max={36} value={size} onChange={e => setSize(e.target.value)} />
+          <span className="brushSizeBg"><Icon icon="circle" style={iconStyle}/></span>
+        </span>
+      </Panel.Body>
+    </Panel>
+  );
+};
 
 export default DrawingControls;
--- client/components/Icon.jsx
+++ client/components/Icon.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Icon({icon}){
+function Icon({icon, style}){
   return (
-    <span className="material-icons">{icon}</span>
+    <span className="material-icons" style={style}>{icon}</span>
   );
 }
 
 Icon.propTypes = {
-  icon: PropTypes.string
+  icon: PropTypes.string.isRequired,
+  style: PropTypes.object
 };
 
 export default Icon;
